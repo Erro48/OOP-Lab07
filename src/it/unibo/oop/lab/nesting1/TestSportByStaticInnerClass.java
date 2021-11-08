39ca4b458b8c5b3e0ab2a62be06e7974ab288a11
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-11-08 11:30:08 +0100

    Add testo for dcassani and becclestone

--- src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
+++ src/it/unibo/oop/lab/nesting1/TestSportByStaticInnerClass.java
@@ -29,8 +29,8 @@ public final class TestSportByStaticInnerClass {
          * 
          * Fernando Alonso, falonso, 34
          */
-        final SportSocialNetworkUserImpl<User> dcassani = null;
-        final SportSocialNetworkUserImpl<User> becclestone = null;
+        final SportSocialNetworkUserImpl<User> dcassani = new SportSocialNetworkUserImpl<>("Davide", "Cassani", "dcassani");
+        final SportSocialNetworkUserImpl<User> becclestone = new SportSocialNetworkUserImpl<>("Bernie", "Ecclestone", "becclestone");
         final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<>("Fernando", "Alonso", "falonso");
 
         /*
