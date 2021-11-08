5c6c4280612559f7d7eb1daa6baa5e21f59ac499
Enrico Tagliaferri <enrico.tagliaferri2@studio.unibo.it>
2021-11-08 12:38:52 +0100

    Change type of set, from User to Sport

--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -33,7 +33,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * add a field to keep track of the set of sports followed/done by a user
      */
 	
-	private Set<User> sports;
+	private Set<Sport> sports;
 
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
@@ -79,7 +79,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+    	
     }
 
     /**
